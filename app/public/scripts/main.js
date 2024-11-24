e69c9c5df478d7399fc4dc37322c1f8731c39970
Ulimativo <ulrich.raab@outlook.com>
2024-11-24 13:21:33 +0000

    added design and boilerplate

---
+++ app/public/scripts/main.js
@@ -0,0 +1,6 @@
+// Wait for DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('JavaScript is loaded and running!');
+    
+    // Add your JavaScript code here
+});
